85ad244bcdb8fd9f9af2e669a417e416a6977107
otto <hz_yuchen@sina.cn>
2017-11-03 15:35:08 +0800

    new:添加示例;

---
+++ sample/App.js
@@ -0,0 +1,221 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    Dimensions
+} from 'react-native';
+
+import {DatePicker, DualItemPicker, MultiItemPicker} from 'react-native-wheel-picker-free'
+
+const {width} = Dimensions.get('window')
+
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+const dateData = []
+
+const timerData = {}
+
+const multiItemPickerData = [
+    [{label: '2011', value: '2011'}, {label: '2012', value: '2012'}, {label: '2013', value: '2013'}, {
+        label: '2014',
+        value: 2014
+    }],
+    [{label: '1月', value: '1'}, {label: '2月', value: '2'}, {label: '3月', value: '3'}, {label: '4月', value: '4'}],
+    [{label: '湖南省', value: '湖南省'}, {label: '广东省', value: '广东省'}, {label: '内蒙古', value: '内蒙古'}]
+]
+const multiItemPickerDefaultData = undefined
+
+// noinspection JSAnnotator
+export default class App extends Component<{}> {
+
+
+    datePickerDialog
+    timePickerDialog
+    multiItemPickerDialog
+
+    constructor() {
+        super()
+        this._initData()
+    }
+
+    _initData = () => {
+
+        for (let i = 1900; i < 2050; i++) {
+            dateData.push(i.toString())
+        }
+
+        const hour = []
+        const minute = []
+        const second = []
+        for (let i = 0; i <= 24; i++) {
+            const valueStr = this._getLabel(i)
+            hour.push({label: `${valueStr}时`, value: `${valueStr}`})
+        }
+        for (let i = 0; i <= 60; i++) {
+            const valueStr = this._getLabel(i)
+            minute.push({label: `${valueStr}分`, value: `${valueStr}`})
+        }
+        for (let i = 0; i <= 60; i++) {
+            const valueStr = this._getLabel(i)
+            second.push({label: `${valueStr}秒`, value: `${valueStr}`})
+        }
+        const time = [hour, minute, second]
+        Object.assign(timerData, {time: time, defaultValue: undefined})
+    }
+
+    _getLabel = (value) => {
+        if (value < 10) {
+            return `0${value}`
+        } else {
+            return `${value}`
+        }
+    }
+
+    render() {
+
+        const {selectedValue = '无选择数据'} = this.state || {}
+
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit App.js
+                </Text>
+                <Text style={styles.instructions}>
+                    {instructions}
+                </Text>
+                <TouchableOpacity onPress={() => this.datePickerDialog.showDialog()}>
+                    <Text style={styles.btn}>日期选择器</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.timePickerDialog.showDialog()}>
+                    <Text style={styles.btn}>时间选择器</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.multiItemPickerDialog.showDialog()}>
+                    <Text style={styles.btn}>自由组合</Text>
+                </TouchableOpacity>
+
+                <Text style={styles.result}>{selectedValue}</Text>
+                <DatePicker
+                    ref={(dialog) => {
+                        this.datePickerDialog = dialog
+                    }}
+                    yearData={dateData}
+                    selectData={new Date}
+                    onChange={this._onPickerConform}
+                    keepShowModal={false}
+                    formatCharacter="-"
+                    cancleText="取消"
+                    finishText="确定"
+                    title="出生年月日"
+                    modalColor="#0000"
+                    itemStyle={{color: 'black', fontSize: 24}}
+                    buttonStyle={{
+                        color: '#003366',
+                        fontSize: 24
+                    }}
+                    titleStyle={{color: 'black', fontSize: 26}}
+                />
+                <MultiItemPicker
+                    ref={(dialog) => this.timePickerDialog = dialog}
+                    dataSet={timerData.time}
+                    defaultSelectedValue={timerData.defaultValue}
+                    onPick={this._onPickerConform}
+                    title="时间选择器"
+                    itemStyle={styles.pickerItem}
+                    buttonStyle={styles.pickerConfirmBtn}
+                    topLineStyle={styles.singleItemPickTopLineStyle}
+                    titleStyle={styles.singleItemPickerTitle}
+                />
+                <MultiItemPicker
+                    ref={(dialog) => this.multiItemPickerDialog = dialog}
+                    dataSet={multiItemPickerData}
+                    defaultSelectedValue={multiItemPickerDefaultData}
+                    onPick={this._onPickerConform}
+                    title="自由组合"
+                    itemStyle={styles.pickerItem}
+                    buttonStyle={styles.pickerConfirmBtn}
+                    topLineStyle={styles.singleItemPickTopLineStyle}
+                    titleStyle={styles.singleItemPickerTitle}
+                />
+            </View>
+        );
+    }
+
+    _onPickerConform = (selectedValue) => {
+        this.setState({selectedValue: JSON.stringify(selectedValue)})
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    btn: {
+        padding: 10,
+        backgroundColor: '#003366',
+        color: '#fff',
+        fontSize: 20,
+        borderRadius: 10,
+        borderWidth: 1,
+        marginTop: 10,
+    },
+    result: {
+        width: width,
+        height: 200,
+        backgroundColor: '#000',
+        color: '#fff',
+        fontSize: 10,
+        marginTop: 20,
+        padding: 10,
+    },
+    singleItemPickerTitle: {
+        color: 'black',
+        fontSize: 26,
+    },
+    pickerItem: {
+        color: 'black',
+        fontSize: 24
+    },
+    pickerConfirmBtn: {
+        color: '#00FF00',
+        fontSize: 24
+    },
+    pickerCancelBtn: {
+        color: 'gray',
+        fontSize: 24
+    },
+    singleItemPickTopLineStyle: {
+        backgroundColor: '#0000FF',
+        height: 1,
+        opacity: 0.8
+    },
+});
